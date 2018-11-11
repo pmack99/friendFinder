92897383cbc611638c166bb087e3862ddf39f998
PMack2 <Prestonmack@gmail.com>
2018-11-11 13:28:19 -0500

    initial

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require("express");
+var http = require("http");
+var fs = require("fs");
+var path = require('path');
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
